17d18d603888aa9c46e3bf44dfaae1701934bc31
mztcandle97 <mayzinthaw97@gmail.com>
2022-06-21 09:05:10 +0630

    update changes

---
+++ src/components/FloatingDiv/Floatingwhatapp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Whatapp from "../../img/whatapp.png";
+import './FloatingDiv.css'
+
+const Floatinwhatapp = ({}) =>{
+    return(
+        <div className="i-icons">
+            <img src={Whatapp}></img>
+        </div>
+    );
+};
+
+export default Floatinwhatapp;
